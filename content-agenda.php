7d6bf983b1034bf00e9dc157bebc46f709cfa203
Dillyn Barber <dillyn@caveconsulting.com>
2017-11-02 21:02:31 -0500

    Add files via upload

---
+++ content-agenda.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Terrell
+ * @since 1.0
+ * @version 1.0
+ */
+?>
+            <?php  
+                query_posts('category_name=Agendas&order=DESC');
+                while (have_posts()) : the_post();
+				
+            ?>
+			
+<!-- The Table -->			
+<table id="postTable">
+<thead>
+  <tr>
+   <th><?php echo the_field('type'); ?></th><th>Meeting</th>
+  </tr>
+</thead>
+<tbody>
+<tr>
+<td><button type="button" data-toggle="modal" data-target="#myModal-<?php $content = the_field('agenda_id', $post->ID); ?>">View Agenda</button></td>
+<td><button type="button" data-toggle="modal" data-target="#myModal-<?php $content = the_field('meeting_date', $post->ID); ?>">View Meeting</button></td>
+</tr>
+</tbody></table>
+<!-- The Table -->
+
+
+<div class="modal" id="myModal-<?php $content = the_field('agenda_id', $post->ID); ?>">
+
+
+  <!-- Agenda Modal content -->
+  <div class="modal-content">
+    <span class="close" data-dismiss="modal" aria-hidden="true">&times;</span>
+    <iframe class="uyd-embedded" src="https://docs.google.com/file/d/<?php $content = the_field('agenda_id', $post->ID); ?>/preview?rm=minimal" width="100%" height="500" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen"></iframe>
+  </div>
+<!-- Agenda Modal content -->
+
+
+</div>
+
+
+
+<div class="modal" id="myModal-<?php $content = the_field('meeting_date', $post->ID); ?>">
+
+
+  <!-- Meeting Modal content -->
+  <div class="modal-content">
+    <span class="close" data-dismiss="modal" aria-hidden="true">&times;</span>
+		
+<iframe class="uyd-embedded" style="display: inline-block;" src="https://www.youtube.com/embed/<?php $content = the_field('youtube_video_id', $post->ID); ?>" name="video" width="40%" height="380" frameborder="0" scrolling="no" align="left" allowfullscreen="allowfullscreen"></iframe>
+
+<iframe class="uyd-embedded" style="display: inline-block;" src="https://docs.google.com/file/d/<?php $content = the_field('agenda_id', $post->ID); ?>/preview?rm=minimal" width="60%" height="500" frameborder="0" scrolling="no" align="right" allowfullscreen="allowfullscreen"></iframe>
+<h3>Index</h3>
+<ul>
+ 	<li><a href="https://www.youtube.com/embed/<?php $content = the_field('youtube_video_id', $post->ID); ?>?start=80&amp;autoplay=1" target="video">1m20s</a> This is a alpha.</li>
+</ul>
+  </div>
+<!-- Meeting Modal content -->
+
+
+</div>
+<script>
+  $(document).ready(function(){  
+    $(".clickme").on
+      ("click", function(){ $("#myModal-" + $(this).attr('data-id')).modal();});
+  });
+</script>
+
+
+            <?php 
+                endwhile;
+                wp_reset_query();
+            ?>
---
+++ tmp_agenda.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Agenda
+ */
+get_header();  ?><script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" /><script type="text/javascript"> jQuery(function(){    jQuery('#postTable').dataTable(); });</script>
+        <figure class="inr_page_sec">
+            <div class="container">
+                <div class="inr_page_container">
+                    <div class="row">
+                		<?php
+                            while ( have_posts() ) : the_post();
+                
+                				get_template_part( 'template-parts/page/content', 'agenda' );
+                
+                			endwhile; // End of the loop.
+                        ?>
+                    </div>
+               </div>
+            </div>
+		</figure>
+    
+<?php
+get_footer();
