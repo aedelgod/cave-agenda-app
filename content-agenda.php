88992a2beb7d884512f7499303220972071c518e
Dillyn Barber <dillyn@caveconsulting.com>
2017-11-03 18:08:32 -0500

    Update content-agenda.php

--- content-agenda.php
+++ content-agenda.php
@@ -1,6 +1,10 @@
-<?php query_posts('category_name=Agendas&order=DESC'); while (have_posts()) : the_post(); ?>
+<?php query_posts('category_name=Agendas&order=DESC'); while (have_posts()) : the_post();
 
-<!-- Begin the Table -->			
+//wp_enqueue_style( 'style', 'https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css' );
+//wp_enqueue_script( 'script', 'https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js' );
+ ?>
+
+<!-- The Table -->			
 <table id="myTable-<?php echo the_field('type'); ?>" class="rows">
 <thead>
   <tr>
@@ -15,32 +19,71 @@
 <td><button type="button" data-toggle="modal" data-target="#myModal-<?php $content = the_field('meeting_date', $post->ID); ?>" class="wpcf7-form-control wpcf7-submit">View Meeting</button></td>
 </tr>
 </tbody></table><br>
-<!-- End the Table -->
+<!-- The Table -->
 
- <!-- Begin Agenda Modal -->
+
 <div class="modal" id="myModal-<?php $content = the_field('agenda_id', $post->ID); ?>">
+
+
   <!-- Agenda Modal content -->
   <div class="modal-content">
     <span class="close" data-dismiss="modal" aria-hidden="true">&times;</span>
     <iframe class="uyd-embedded" src="https://docs.google.com/file/d/<?php $content = the_field('agenda_id', $post->ID); ?>/preview?rm=minimal" width="100%" height="500" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen"></iframe>
   </div>
 <!-- Agenda Modal content -->
-</div> <!-- End Modal -->
 
- <!-- Begin Meeting Modal -->
+
+</div>
+
+
+
 <div class="modal" id="myModal-<?php $content = the_field('meeting_date', $post->ID); ?>">
+
+
   <!-- Meeting Modal content -->
   <div class="modal-content">
     <span class="close" data-dismiss="modal" aria-hidden="true">&times;</span>
-		
-<iframe class="uyd-embedded" style="display: inline-block; padding-top: inherit; padding-right: 10px;" src="https://www.youtube.com/embed/<?php $content = the_field('youtube_video_id', $post->ID); ?>" name="video-<?php $content = the_field('youtube_video_id', $post->ID); ?>" width="40%" height="380" frameborder="0" scrolling="no" align="left" allowfullscreen="allowfullscreen"></iframe>
 
-<iframe class="uyd-embedded" style="display: inline-block; float: inherit;" src="https://docs.google.com/file/d/<?php $content = the_field('agenda_id', $post->ID); ?>/preview?rm=minimal" width="60%" height="500" frameborder="0" scrolling="no" align="right" allowfullscreen="allowfullscreen"></iframe><br>
-<h3>Index</h3>
-<ul>
- 	<li><a href="https://www.youtube.com/embed/<?php $content = the_field('youtube_video_id', $post->ID); ?>?start=30&amp;autoplay=1" target="video-<?php $content = the_field('youtube_video_id', $post->ID); ?>">30s</a> This is a concept.</li>
-</ul>
+<div style="display: inline-block;padding-right: 10px; width:38%">
+	
+<iframe class="uyd-embedded" style="" src="https://www.youtube.com/embed/<?php $content = the_field('youtube_video_id', $post->ID); ?>?rel=0&enablejsapi=1" name="video-<?php $content = the_field('youtube_video_id', $post->ID); ?>" width="100%" height="380" frameborder="0" scrolling="no" align="left" allowfullscreen="allowfullscreen"></iframe>
+<br>
+<span>Index</span><br>
+<a href="https://www.youtube.com/embed/<?php $content = the_field('youtube_video_id', $post->ID); ?>?start=30&autoplay=1&rel=0&enablejsapi=1" target="video-<?php $content = the_field('youtube_video_id', $post->ID); ?>">30s</a> This is a concept.</div>
+
+
+<iframe class="uyd-embedded" style="display: inline-block; float: inherit;" src="https://docs.google.com/file/d/<?php $content = the_field('agenda_id', $post->ID); ?>/preview?rm=minimal" width="60%" height="500" frameborder="0" scrolling="no" align="right" allowfullscreen="allowfullscreen"></iframe>
+
+
+
+
   </div>
+
+
 <!-- Meeting Modal content -->
-</div> <!-- End Modal -->
-            <?php /* It loops derp */ endwhile; wp_reset_query(); ?>
+
+
+</div>
+<script>
+
+<!-- <script> -->
+ // $(document).ready(function(){  
+ //   $(".clickme").on
+ //     ("click", function(){ $("#myModal-" + $(this).attr('data-id')).modal();});
+ // }); 
+</script> 
+
+            <?php 
+                endwhile;
+                wp_reset_query();
+            ?>
+<script>
+$(function(){
+  $("body").on('hidden.bs.modal', function (e) {
+    var $iframes = $(e.target).find("iframe");
+    $iframes.each(function(index, iframe){
+      $(iframe).attr("src", $(iframe).attr("src"));
+    });
+  });
+});
+</script>			
