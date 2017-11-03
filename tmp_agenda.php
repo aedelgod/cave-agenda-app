3593e7e46be942cdb66ee45ba52932328193131b
Dillyn Barber <dillyn@caveconsulting.com>
2017-11-03 00:33:53 -0500

    Update tmp_agenda.php

--- tmp_agenda.php
+++ tmp_agenda.php
@@ -1,50 +1,10 @@
-<?php
-
-get_header(); 
-
- ?>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" />
-<script type="text/javascript">
- jQuery(function(){
-    jQuery('#postTable').dataTable();
- });
-</script>
-
-
-
+<?php get_header();  ?>
         <figure class="inr_page_sec">
-
             <div class="container">
-
                 <div class="inr_page_container">
-
                     <div class="row">
-
-                		<?php
-
-                            while ( have_posts() ) : the_post();
-
-                
-
-                				get_template_part( 'template-parts/page/content', 'agenda' );
-
-                
-
-                			endwhile; // End of the loop.
-
-                        ?>
-
-                    </div>
-
-               </div>
-
-            </div>
-
-		</figure>
-
-    
-
-<?php
-
-get_footer();
-
+<?php  while ( have_posts() ) : the_post();  get_template_part( 'template-parts/page/content', 'agenda' ); endwhile; 
+  // End of the loop. Let's not be pros.
+  ?>
+</div> </div></div></figure>
+<?php get_footer();
